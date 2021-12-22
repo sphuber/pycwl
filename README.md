e7c876c61a47b18c320d0684b51f86f57ba2ec7a
Sebastiaan Huber <mail@sphuber.net>
2021-12-22 17:54:38 +0100

    Initial commit
    Skeleton of new package.

---
+++ README.md
@@ -0,0 +1,3 @@
+# `pycwl`
+
+Python package to work with [Common Workflow Language](https://www.commonwl.org/).
---
+++ pycwl/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Python package to work with Common Workflow Language."""
+__version__ = '0.1.0'
